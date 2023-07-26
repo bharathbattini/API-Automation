8575eba61372f833e402860dda2d38eb9eb3497e
Bharath Battini <90249031+bharath712@users.noreply.github.com>
2023-07-26 16:57:38 +0530

    EOS::First Commit::Bharath Battini

---
+++ cypress/e2e/APIAutomation/UnitTestScript/get_user_profile.cy.js
@@ -0,0 +1,25 @@
+/// <reference types = "Cypress" />
+import 'cypress-plugin-api'
+
+let getCustomerProfile = 'https://uat.eos.onefin.app/api/customer/profile?';
+let usercode = 'a4b78d76-713a-46b9-a459-dda29f776470'
+
+
+it('Get Customer Profile', ()=>{
+
+    cy.api({
+
+        method: 'GET',
+        url: getCustomerProfile+'usercode='+usercode,
+        qs:{
+
+            user_code : usercode
+        }
+
+    }).then((response)=>{
+
+        expect(response.status).to.be.eq(200)
+
+    });
+
+});
