a715cfb44b91b70610091aa751ab77ff0291d9ae
Bharath Battini <90249031+bharath712@users.noreply.github.com>
2023-08-31 16:13:28 +0530

    mochawesome reporting

--- cypress.config.js
+++ cypress.config.js
@@ -2,12 +2,14 @@ const { defineConfig } = require("cypress");
 const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   watchForFileChanges: false,
   port: 4200,
   video:false,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
